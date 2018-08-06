3dcc72d27af2a804bfbf57d57cd826c5811d0138
Rowan Schischka <rowanschischka@gmail.com>
2018-08-07 07:19:14 +1200

    Initial commit & repo setup

---
+++ app/src/main/java/com/rowanschischka/rowansintervaltimer/RunningTimerActivity.java
@@ -0,0 +1,12 @@
+package com.rowanschischka.rowansintervaltimer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RunningTimerActivity extends AppCompatActivity {
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.running_timer);
+}}
