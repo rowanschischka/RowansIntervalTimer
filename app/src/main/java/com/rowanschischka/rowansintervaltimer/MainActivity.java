6ef395ddf9e654e0e121eee7b30241fee874b1a1
Rowan Schischka <rowanschischka@gmail.com>
2018-08-13 13:37:17 +1200

    Initial commit & repo setup

--- app/src/main/java/com/rowanschischka/rowansintervaltimer/MainActivity.java
+++ app/src/main/java/com/rowanschischka/rowansintervaltimer/MainActivity.java
@@ -1,34 +1,66 @@
 package com.rowanschischka.rowansintervaltimer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MIN1= 0;
-    private static final int SEC1=1;
-    private static final int MIN2=2;
-    private static final int SEC2=3;
 
-    private NumberPicker[] intervalValues= new NumberPicker[4];
+    NumberPicker NumberPickerMinute1;
+    NumberPicker NumberPickerSecond1;
+    NumberPicker NumberPickerMinute2;
+    NumberPicker NumberPickerSecond2;
+
+    String saved_MIN1;
+    String saved_SEC1;
+    String saved_MIN2;
+    String saved_SEC2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intervalValues[MIN1] = findViewById(R.id.min1);
-        intervalValues[SEC1] = findViewById(R.id.sec1);
-        intervalValues[MIN2] = findViewById(R.id.min2);
-        intervalValues[SEC2] = findViewById(R.id.sec2);
 
-        for(NumberPicker np : intervalValues){
+        saved_MIN1 = getString(R.string.saved_MIN1);
+        saved_SEC1 = getString(R.string.saved_SEC1);
+        saved_MIN2 = getString(R.string.saved_MIN2);
+        saved_SEC2 = getString(R.string.saved_SEC2);
+        NumberPicker intervalValues[] = {
+                NumberPickerMinute1 = findViewById(R.id.min1),
+                NumberPickerSecond1 = findViewById(R.id.sec1),
+                NumberPickerMinute2 = findViewById(R.id.min2),
+                NumberPickerSecond2 = findViewById(R.id.sec2),
+        };
+
+        for (NumberPicker np : intervalValues) {
             np.setMaxValue(59);
             np.setMinValue(0);
         }
+
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.saved_time), Context.MODE_PRIVATE);
+        NumberPickerMinute1.setValue(sharedPref.getInt(saved_MIN1, 0));
+        NumberPickerSecond1.setValue(sharedPref.getInt(saved_SEC1, 30));
+        NumberPickerMinute2.setValue(sharedPref.getInt(saved_MIN2, 0));
+        NumberPickerSecond2.setValue(sharedPref.getInt(saved_SEC2, 10));
     }
 
     public void onClickStart(View view) {
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.saved_time), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(saved_MIN1, NumberPickerMinute1.getValue());
+        //editor.apply();
+        editor.putInt(saved_SEC1, NumberPickerSecond1.getValue());
+        //editor.apply();
+        editor.putInt(saved_MIN2, NumberPickerMinute2.getValue());
+        //editor.apply();
+        editor.putInt(saved_SEC2, NumberPickerSecond2.getValue());
+        editor.apply();
 
+        Intent myIntent = new Intent(MainActivity.this, RunningTimerActivity.class);
+        this.startActivity(myIntent);
     }
 }
--- app/src/main/java/com/rowanschischka/rowansintervaltimer/RunningTimerActivity.java
+++ app/src/main/java/com/rowanschischka/rowansintervaltimer/RunningTimerActivity.java
@@ -1,12 +1,123 @@
 package com.rowanschischka.rowansintervaltimer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RunningTimerActivity extends AppCompatActivity {
+    public Handler setTimeTextHandler = new Handler(Looper.getMainLooper()) {
+        public void handleMessage(Message msg) {
+            TextView timeText = findViewById(R.id.timeText);
+            long time = (long) msg.obj;
+            long seconds = time / 1000;
+            long milli = (time - seconds * 1000) / 100;
+            String text = seconds + "." + milli;
+            timeText.setText(text);
+        }
+    };
+    int updatePeriod = 100;
+    long startTime;
+    int time1Millis;
+    int time2Millis;
+    int currentIntervalTime;
+    boolean interval = true;
+    ToneGenerator tg;
+    public Handler switchIntervalHandler = new Handler(Looper.getMainLooper()) {
+        public void handleMessage(Message msg) {
+            LinearLayout layout = findViewById(R.id.runningTimer);
+            startTime = SystemClock.uptimeMillis();
+            if (interval) {
+                currentIntervalTime = time2Millis;
+                layout.setBackgroundColor(getResources().getColor(R.color.time2));
+                tg.startTone(ToneGenerator.TONE_PROP_BEEP2);
+            } else {
+                currentIntervalTime = time1Millis;
+                layout.setBackgroundColor(getResources().getColor(R.color.time1));
+                tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+            }
+            interval = !interval;
+        }
+    };
+    Timer timer;
 
-@Override
+    @Override
+    protected void onPause() {
+        timer.cancel();
+        super.onPause();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.running_timer);
-}}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.running_timer);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, 100);
+
+        String saved_MIN1 = getString(R.string.saved_MIN1);
+        String saved_SEC1 = getString(R.string.saved_SEC1);
+        String saved_MIN2 = getString(R.string.saved_MIN2);
+        String saved_SEC2 = getString(R.string.saved_SEC2);
+
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.saved_time), Context.MODE_PRIVATE);
+        int min1 = (sharedPref.getInt(saved_MIN1, 0));
+        int sec1 = (sharedPref.getInt(saved_SEC1, 30));
+        time1Millis = (min1 * 60 + sec1) * 1000;
+        int min2 = (sharedPref.getInt(saved_MIN2, 0));
+        int sec2 = (sharedPref.getInt(saved_SEC2, 10));
+        time2Millis = (min2 * 60 + sec2) * 1000;
+
+        currentIntervalTime = time1Millis;
+
+        startTime = SystemClock.uptimeMillis();
+
+        TimerTask timerTask = new MyTimerTask();
+        timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, updatePeriod);
+    }
+
+    public void onClickStop(View view) {
+        finish();
+    }
+
+    private class MyTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            long upTime = SystemClock.uptimeMillis();
+            long elapsedTimeMilli = upTime - startTime;
+
+            long displayTime = currentIntervalTime - elapsedTimeMilli;
+
+            Log.d("TIME",
+                    "elapsedTimeMilli " + elapsedTimeMilli +
+                            " displayTime " + displayTime +
+                            "currentIntervalTime " + currentIntervalTime
+            );
+            if (displayTime >= 0) {
+                Message displayMessage = setTimeTextHandler.obtainMessage(0, displayTime);
+                displayMessage.sendToTarget();
+            }
+            //swap time interval
+            if (displayTime <= 0) {
+                Message switchInterval = switchIntervalHandler.obtainMessage();
+                switchInterval.sendToTarget();
+            }
+        }
+    }
+}
